13c4c554b165237aa9c39e1a606c07b7a66bea68
Bunyamin Gokcen <bnymn.gkcn2@gmail.com>
2024-07-28 15:52:49 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         int[] list = {1, 2, 3, 4, 5};
-        double total = 0;
+        double total = 1;
 
         for (int i = 0; i < list.length; i++){
             total += ((double) 1 /list[i]);
         }
-        total += 1;
+
         System.out.println(total);
     }
 }
